bc7040098e1782c98fecba4cd8d2fb410bd492f3
vishalmanglaw453 <vishalmanglaw@gmail.com>
2024-05-18 09:47:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import New from "./componets/New";
+import Todo from "./componets/Todo";
+
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        <New/>
+        <Todo/>
+      </>
+    </div>
+  );
+}
+
+export default App;
